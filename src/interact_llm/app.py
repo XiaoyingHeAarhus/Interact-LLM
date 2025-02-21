2b2cdf074e8a1f8f660a5513789b0dba787f29a5
MinaAlmasi <mina.almasi00@gmail.com>
2025-02-21 11:22:15 +0100

    feat: add modal screen for quitting instead of quitting from command palette to allow for saving a file each time the app is quit of chat messages

--- src/interact_llm/app.py
+++ src/interact_llm/app.py
@@ -3,6 +3,9 @@ Initial inspiration:https://textual.textualize.io/blog/2024/09/15/anatomy-of-a-t
 """
 
 from pathlib import Path
+from typing import Optional
+from datetime import datetime
+import json
 
 from llm.hf_wrapper import ChatHF
 from llm.mlx_wrapper import ChatMLX
@@ -10,11 +13,32 @@ from data_models.chat import ChatHistory, ChatMessage
 
 from textual import on, work
 from textual.app import App, ComposeResult
-from textual.containers import VerticalScroll
-from textual.widgets import Footer, Header, Input, Markdown
+from textual.containers import VerticalScroll, Grid
+from textual.screen import ModalScreen
+from textual.widgets import Footer, Input, Markdown, Button, Label
 from transformers.utils.logging import disable_progress_bar
 
 disable_progress_bar()
+
+class QuitScreen(ModalScreen[bool]):  
+    """
+    Screen with a dialog to quit !!
+    From: https://textual.textualize.io/guide/screens/#__tabbed_4_4
+    """
+
+    def compose(self) -> ComposeResult:
+        yield Grid(
+            Label("Are you sure you want to quit?", id="question"),
+            Button("Quit", variant="error", id="quit"),
+            Button("Cancel", variant="primary", id="cancel"),
+            id="dialog",
+        )
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "quit":
+            self.dismiss(True)
+        else:
+            self.dismiss(False)
 
 # classes for formatting
 class UserMessage(Markdown):
@@ -24,14 +48,15 @@ class UserMessage(Markdown):
 class Response(Markdown):
     BORDER_TITLE = "Interact-LLM"
 
-
-class ChatApp(App):
+class ChatApp(App[ChatHistory]):
     """
     Texttual app for chatting with llm
     """
 
     AUTO_FOCUS = "INPUT"
+    ENABLE_COMMAND_PALETTE = False
 
+    BINDINGS = [("q", "request_quit", "Quit")]
     CSS = """
     UserMessage {
         background: $primary 10%;
@@ -49,30 +74,74 @@ class ChatApp(App):
         margin-left: 8; 
         padding: 1 2 0 2;
     }
+
+    QuitScreen {
+        align: center middle;
+    }
+
+    #dialog {
+        grid-size: 2;
+        grid-gutter: 1 2;
+        grid-rows: 1fr 3;
+        padding: 0 1;
+        width: 60;
+        height: 11;
+        border: thick $background 80%;
+        background: $surface;
+    }
+
+    #question {
+        column-span: 2;
+        height: 1fr;
+        width: 1fr;
+        content-align: center middle;
+    }
+
+    Button {
+        width: 100%;
+    }
     """
 
-    def __init__(self, model:ChatHF|ChatMLX):
+    def __init__(self, model:ChatHF|ChatMLX, chat_messages_dir: Optional[Path] = None):
         super().__init__()
         self.chat_history = ChatHistory(messages=[])
         self.model = model
+        self.chat_messages_dir = chat_messages_dir
 
-    '''
-    def on_mount(self) -> None:
-        #self.model = ChatHF(model_id=MODEL_ID, cache_dir=CACHE_DIR, device=DEVICE)
-        self.model = ChatMLX(model_id=MODEL_ID)
-        self.model.load()
-    '''
+        if self.chat_messages_dir is not None:
+            self._ensure_chat_dir_exists()
+
+    # wrangling chat messages 
+    def _ensure_chat_dir_exists(self):
+        self.chat_messages_dir.mkdir(parents=True, exist_ok=True)
+
     def update_chat_history(self, chat_message: ChatMessage) -> None:
         """Update chat history with a single new message."""
         self.chat_history.messages.append(chat_message)
 
+    # app buttons 
     def compose(self) -> ComposeResult:
-        yield Header()
         with VerticalScroll(id="chat-view"):
             yield Response("¿Hola quieres practicar conmigo?")
         yield Input(placeholder="Escribe tu mensaje aquí")
         yield Footer()
 
+    def action_request_quit(self) -> None:
+        """Action to display the quit dialog."""
+
+        def check_quit(quit: bool | None) -> None:
+            """Called when QuitScreen is dismissed."""
+            if quit:
+                if self.chat_messages_dir is not None:
+                    chat_json = json.dumps([msg.dict() for msg in self.chat_history.messages], indent=3, ensure_ascii=False)
+                    save_file_name = datetime.now().strftime("%Y%m%d-%H%M%S")
+                    with open(self.chat_messages_dir / f"{save_file_name}.json", "w") as outfile:
+                        outfile.write(chat_json)
+                self.exit()
+
+        self.push_screen(QuitScreen(), check_quit)
+
+    ## chatting functionality 
     @on(Input.Submitted)
     async def on_input(self, user_message: Input.Submitted) -> None:
         chat_view = self.query_one("#chat-view")
@@ -120,8 +189,11 @@ def main():
         model = ChatHF(model_id=model_id, cache_dir=cache_dir)
         model.load()
 
+    # define save dir 
+    save_dir = Path(__file__).parents[3] / "data" / model_id.replace("/", "--")
+
     # open tui app
-    app = ChatApp(model=model)
+    app = ChatApp(model=model, chat_messages_dir=save_dir)
     app.run()
 
 
