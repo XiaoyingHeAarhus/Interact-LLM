7dce9ab03f50e1bc8280d6ce04ad907946310e0b
MinaAlmasi <mina.almasi00@gmail.com>
2025-03-08 13:51:49 +0100

    feat: example pipeline bash script

--- src/scripts/simulate.sh
+++ src/scripts/simulate.sh
@@ -1,3 +1,11 @@
 for run in {1..10}; do
-    uv run python simulate.py
+    uv run python src/scripts/simulate.py --prompt_id "A1"
+done
+
+for run in {1..10}; do
+    uv run python src/scripts/simulate.py --prompt_id "B1"
+done
+
+for run in {1..10}; do
+    uv run python src/scripts/simulate.py --prompt_id "C1"
 done
