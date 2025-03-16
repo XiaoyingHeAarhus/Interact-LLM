577ede71db6042a1007978e8cf1d22674fbffff9
MinaAlmasi <mina.almasi00@gmail.com>
2025-03-16 14:10:20 +0100

    update simulate to work with new model loading backend system

--- src/scripts/simulate.sh
+++ src/scripts/simulate.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-models=("Qwen/Qwen2.5-7B-Instruct")
+models=("llama3.1:8b" "qwen2.5:7b")
 prompt_ids=("A1" "B1" "C1")
+backend="hf"
 
 for model in "${models[@]}"; do
     for prompt_id in "${prompt_ids[@]}"; do
-            uv run python src/scripts/simulate.py --model "$model" --prompt_id "$prompt_id"
+            uv run python src/scripts/simulate.py --model_name "$model" --prompt_id "$prompt_id" --backend "$backend"
     done
 done
